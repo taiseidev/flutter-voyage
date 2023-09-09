acbc9547a2427bce5ea828c59f5364346032e9d0
onishi taisei <taisei.developer@gmail.com>
2023-09-10 02:37:28 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_voyage/features/screenTransitionInModal/home_page.dart';
+import 'package:flutter_voyage/features/chart/home_page.dart';
 
 void main() {
   runApp(const MyApp());
