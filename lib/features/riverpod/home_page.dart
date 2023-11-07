781312927eb0750a7ba6481577deb4b2d6257002
onishi taisei <taisei.developer@gmail.com>
2023-11-07 21:33:15 +0900

    add

--- lib/features/riverpod/home_page.dart
+++ lib/features/riverpod/home_page.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class HomePage extends StatelessWidget {
+part 'home_page.g.dart';
+
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container();
   }
+}
+
+class Item {
+  const Item(
+    this.id,
+    this.title,
+    this.price,
+  );
+
+  final String id;
+  final String title;
+  final int price;
+}
+
+class CartListPage extends ConsumerWidget {
+  const CartListPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      children: [],
+    );
+  }
+}
+
+@Riverpod(dependencies: [])
+Item currentItem(CurrentItemRef ref) => throw UnimplementedError();
+
+@riverpod
+Item bargenItem(BargenItemRef ref) {
+  return ref.watch(currentItemProvider);
+}
+
+class CartItem extends ConsumerWidget {
+  const CartItem({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final item = ref.watch(currentItemProvider);
+    return ListTile(
+      title: Text(item.title),
+    );
+  }
+}
+
+// A non-scoped provider
+@riverpod
+int root(RootRef ref) => 0;
+
+// A possibly scoped provider
+@Riverpod(dependencies: [])
+int scoped(ScopedRef ref) => 0;
+
+// -----------
+
+// No dependencies used, no need to specify "dependencies"
+@riverpod
+int example1(Example1Ref ref) => 0;
+
+// We can specify an empty "dependencies" list if we wish to.
+// This marks the provider as "scoped".
+@Riverpod(dependencies: [])
+int example2(Example2Ref ref) => 0;
+
+@riverpod
+int example3(Example3Ref ref) {
+  // rootProvider is not scoped, no need to specify it as "dependencies"
+  return ref.watch(rootProvider);
+}
+
+@Riverpod(dependencies: [scoped])
+int example4(Example4Ref ref) {
+  // scopedProvider is scoped and as such specifying "dependencies" is required.
+  return ref.watch(scopedProvider);
+}
+
+// -----------
+
+// scopedProvider isn't used and should therefore not be listed
+@Riverpod(dependencies: [scoped])
+int example5(Example5Ref ref) => 0;
+
+@Riverpod(dependencies: [])
+int example6(Example6Ref ref) {
+  // scopedProvider is used but not present in the list of dependencies
+  return ref.watch(scopedProvider);
+}
+
+@Riverpod(dependencies: [root])
+int example7(Example7Ref ref) {
+  // rootProvider is not a scoped provider. As such it shouldn't be listed in "dependencies"
+  return ref.watch(rootProvider);
 }
---
+++ lib/features/riverpod/home_page.g.dart
@@ -0,0 +1,173 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_page.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currentItemHash() => r'd2eeab1ac3d7862fdaadaf37d102bbaaea4c3ca4';
+
+/// See also [currentItem].
+@ProviderFor(currentItem)
+final currentItemProvider = AutoDisposeProvider<Item>.internal(
+  currentItem,
+  name: r'currentItemProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$currentItemHash,
+  dependencies: const <ProviderOrFamily>[],
+  allTransitiveDependencies: const <ProviderOrFamily>{},
+);
+
+typedef CurrentItemRef = AutoDisposeProviderRef<Item>;
+String _$bargenItemHash() => r'a5b0ce6502761e77261faa51f594e7d6093bd6cf';
+
+/// See also [bargenItem].
+@ProviderFor(bargenItem)
+final bargenItemProvider = AutoDisposeProvider<Item>.internal(
+  bargenItem,
+  name: r'bargenItemProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$bargenItemHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef BargenItemRef = AutoDisposeProviderRef<Item>;
+String _$rootHash() => r'1cd85d73316aad02169ff0f5e7af5cf1423410ff';
+
+/// See also [root].
+@ProviderFor(root)
+final rootProvider = AutoDisposeProvider<int>.internal(
+  root,
+  name: r'rootProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$rootHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RootRef = AutoDisposeProviderRef<int>;
+String _$scopedHash() => r'bbf25968b1186d2dd63d10545364453712d491cf';
+
+/// See also [scoped].
+@ProviderFor(scoped)
+final scopedProvider = AutoDisposeProvider<int>.internal(
+  scoped,
+  name: r'scopedProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$scopedHash,
+  dependencies: const <ProviderOrFamily>[],
+  allTransitiveDependencies: const <ProviderOrFamily>{},
+);
+
+typedef ScopedRef = AutoDisposeProviderRef<int>;
+String _$example1Hash() => r'b7682e52c869aeaf5a51c8b186fa1e194750ebfb';
+
+/// See also [example1].
+@ProviderFor(example1)
+final example1Provider = AutoDisposeProvider<int>.internal(
+  example1,
+  name: r'example1Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example1Hash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef Example1Ref = AutoDisposeProviderRef<int>;
+String _$example2Hash() => r'a5651cf43300b11b3e75d5e39a3ce640d94c9beb';
+
+/// See also [example2].
+@ProviderFor(example2)
+final example2Provider = AutoDisposeProvider<int>.internal(
+  example2,
+  name: r'example2Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example2Hash,
+  dependencies: const <ProviderOrFamily>[],
+  allTransitiveDependencies: const <ProviderOrFamily>{},
+);
+
+typedef Example2Ref = AutoDisposeProviderRef<int>;
+String _$example3Hash() => r'ab11c67bde5a8208cae84d954970bdf54d39093a';
+
+/// See also [example3].
+@ProviderFor(example3)
+final example3Provider = AutoDisposeProvider<int>.internal(
+  example3,
+  name: r'example3Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example3Hash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef Example3Ref = AutoDisposeProviderRef<int>;
+String _$example4Hash() => r'a9aa63cd3c15e496eb83c72b960f14b37bdf3202';
+
+/// See also [example4].
+@ProviderFor(example4)
+final example4Provider = AutoDisposeProvider<int>.internal(
+  example4,
+  name: r'example4Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example4Hash,
+  dependencies: <ProviderOrFamily>[scopedProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    scopedProvider,
+    ...?scopedProvider.allTransitiveDependencies
+  },
+);
+
+typedef Example4Ref = AutoDisposeProviderRef<int>;
+String _$example5Hash() => r'd393263b65ea4b72fedaf0f25582f97bfb95ab5f';
+
+/// See also [example5].
+@ProviderFor(example5)
+final example5Provider = AutoDisposeProvider<int>.internal(
+  example5,
+  name: r'example5Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example5Hash,
+  dependencies: <ProviderOrFamily>[scopedProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    scopedProvider,
+    ...?scopedProvider.allTransitiveDependencies
+  },
+);
+
+typedef Example5Ref = AutoDisposeProviderRef<int>;
+String _$example6Hash() => r'0355af3d5773d0dd16c279d7a2d61fe4ee4a4818';
+
+/// See also [example6].
+@ProviderFor(example6)
+final example6Provider = AutoDisposeProvider<int>.internal(
+  example6,
+  name: r'example6Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example6Hash,
+  dependencies: const <ProviderOrFamily>[],
+  allTransitiveDependencies: const <ProviderOrFamily>{},
+);
+
+typedef Example6Ref = AutoDisposeProviderRef<int>;
+String _$example7Hash() => r'562839821147ecd9f7858303b1f9561c0ef9b78d';
+
+/// See also [example7].
+@ProviderFor(example7)
+final example7Provider = AutoDisposeProvider<int>.internal(
+  example7,
+  name: r'example7Provider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$example7Hash,
+  dependencies: <ProviderOrFamily>[rootProvider],
+  allTransitiveDependencies: <ProviderOrFamily>{
+    rootProvider,
+    ...?rootProvider.allTransitiveDependencies
+  },
+);
+
+typedef Example7Ref = AutoDisposeProviderRef<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
